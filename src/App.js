16540ec06507b16b82e69a405ac70b7ca0f42abd
Silas <silasfas@hotmail.com>
2023-06-14 23:30:31 -0300

    update

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import './App.css';
 
 function App() {
 
-
-
-
   return (
     <div className="App">
-      
-    <h1>Hello there, Start here!</h1>
+      <h1>Hello there, Start here!</h1>
     </div>
   );
 }
