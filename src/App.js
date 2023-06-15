e19fc8523020b2ff197fabcf6bf4cc6c92265a09
Silas <silasfas@hotmail.com>
2023-06-14 23:50:16 -0300

    update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import './App.css';
 
 function App() {
 
+  const handleNameChange = () => {
+    const names = ['Jake', 'Kevin', 'Dave', 'Silas']
+    const int = Math.floor(Math.random() * 4)
+    return names[int]
+  }
+
   return (
     <div className="App">
       <h1>Hello there, Start here!</h1>
+      <h2>{handleNameChange()}</h2>
+
     </div>
   );
 }
