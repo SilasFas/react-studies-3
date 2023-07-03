a812c83b38f7d0ed1c85824450d944c6510e6a6a
Silas <silasfas@hotmail.com>
2023-07-03 16:25:20 -0300

    click events

--- src/Content.js
+++ src/Content.js
@@ -5,9 +5,26 @@ export const Content = () => {
         const int = Math.floor(Math.random() * 3)
         return names[int]
     }
+
+    const handleClick = () => {
+        console.log('Silas')
+    }
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`)
+    }
+    const handleClick3 = (event) => {
+        console.log(event.target.innerText)
+    }
+
+
+
     return (
         <main>
-            <p>Hello {handleNameChange()}</p>
+            <p onDoubleClick={handleClick}>Hello {handleNameChange()}</p>
+            <button onClick={handleClick}>Click it</button>
+            <button onClick={() => handleClick2('Pedro')}>Click it</button>
+            <button onClick={(event) => handleClick3(event)}>Click it</button>
         </main>
     )
 }
